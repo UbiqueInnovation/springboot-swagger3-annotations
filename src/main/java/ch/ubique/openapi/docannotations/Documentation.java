42c23ddefedce6ac48d7e67ffece2e4987993e35
Patrick Amrein <amrein@ubique.ch>
2020-04-09 10:59:57 +0200

    initial commit

---
+++ src/main/java/ch/ubique/openapi/docannotations/Documentation.java
@@ -0,0 +1,14 @@
+package ch.ubique.openapi.docannotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+@Target({ElementType.METHOD, ElementType.TYPE, ElementType.TYPE_PARAMETER, ElementType.PARAMETER, ElementType.FIELD, ElementType.TYPE_USE})
+@Retention(RetentionPolicy.CLASS)
+public @interface Documentation {
+    String description() default "";
+    String example() default "";
+    String[] responses() default {};
+    boolean undocumented() default false;
+} 
