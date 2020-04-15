ae97c7fd816af2944cf20e8c67c5671ba5d55f66
Patrick Amrein <amrein@ubique.ch>
2020-04-15 13:42:26 +0200

    add serialized class

--- src/main/java/ch/ubique/openapi/docannotations/Documentation.java
+++ src/main/java/ch/ubique/openapi/docannotations/Documentation.java
@@ -3,6 +3,9 @@ package ch.ubique.openapi.docannotations;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+
+import javax.lang.model.type.NullType;
+
 import java.lang.annotation.ElementType;
 @Target({ElementType.METHOD, ElementType.TYPE, ElementType.TYPE_PARAMETER, ElementType.PARAMETER, ElementType.FIELD, ElementType.TYPE_USE})
 @Retention(RetentionPolicy.RUNTIME)
@@ -11,4 +14,5 @@ public @interface Documentation {
     String example() default "";
     String[] responses() default {};
     boolean undocumented() default false;
+    Class<?> serializedClass() default NullType.class ;
 } 
