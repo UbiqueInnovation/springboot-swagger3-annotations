a39c26664e42c63a414c44fdd1ea4e1aed880895
Patrick Amrein <amrein@ubique.ch>
2020-11-24 11:17:33 +0100

    refactor + add response headers

--- src/main/java/ch/ubique/openapi/docannotations/Documentation.java
+++ src/main/java/ch/ubique/openapi/docannotations/Documentation.java
@@ -1,18 +1,31 @@
 package ch.ubique.openapi.docannotations;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import javax.lang.model.type.NullType;
 
-import java.lang.annotation.ElementType;
-@Target({ElementType.METHOD, ElementType.TYPE, ElementType.TYPE_PARAMETER, ElementType.PARAMETER, ElementType.FIELD, ElementType.TYPE_USE})
+@Target({
+    ElementType.METHOD,
+    ElementType.TYPE,
+    ElementType.TYPE_PARAMETER,
+    ElementType.PARAMETER,
+    ElementType.FIELD,
+    ElementType.TYPE_USE
+})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Documentation {
     String description() default "";
+
     String example() default "";
+
     String[] responses() default {};
+
     boolean undocumented() default false;
-    Class<?> serializedClass() default NullType.class ;
-} 
+
+    Class<?> serializedClass() default NullType.class;
+
+    String[] responseHeaders() default {};
+}
